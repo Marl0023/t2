94aff1d702ad3f8f2bf388a08252109fb69fd9f8
Marl0023 <marlotay2312@gmail.com>
2026-05-26 13:03:11 -0500

    first commit

---
+++ ArbolBBP.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Hosting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    internal class ArbolBBP
+    {
+        private NodoP Primero { get; set; }
+        public void Agregar(int codigo, string nombre, int antiguedad)
+        {
+            Primero = InsertarRecursivo(Primero,codigo, nombre, antiguedad);
+        }
+        private NodoP InsertarRecursivo(NodoP r, int codigo, string nombre, int antiguedad)
+        {
+            if (r == null)
+            {
+                Empleado Emp = new Empleado(codigo,nombre,antiguedad);
+                return new NodoP(Emp);
+            }
+            else if (antiguedad < r.Valor.Antiguedad)
+            {
+                r.Izqu = InsertarRecursivo(r.Izqu, codigo, nombre, antiguedad);
+            }
+            else if (antiguedad > r.Valor.Antiguedad)
+            {
+                r.Dere = InsertarRecursivo(r.Dere, codigo, nombre, antiguedad);
+            }
+            return r;
+        }
+        public void MuestraInCodigo()
+        {
+            MuestraInCodigo(Primero);
+        }
+        private void MuestraInCodigo(NodoP r)
+        {
+            if (r != null)
+            {
+
+                MuestraInCodigo(r.Izqu);
+                Console.Write(r.Valor.Codigo + " ");
+                MuestraInCodigo(r.Dere);
+            }
+        }
+        public void MuestraPosAntiguedad()
+        {
+            MuestraPosAntiguedad(Primero);
+        }
+        private void MuestraPosAntiguedad(NodoP r)
+        {
+            if (r != null)
+            {
+                MuestraPosAntiguedad(r.Izqu);
+                MuestraPosAntiguedad(r.Dere);
+                Console.Write(r.Valor.Antiguedad + " ");
+            }
+        }
+        public void MayoresA(int antiguedad) {
+            if (Primero == null)
+                Console.WriteLine("El árbol está vacío.");
+            else {
+                Buscarrec(Primero, antiguedad);
+                
+            }
+            
+        }
+        private void Buscarrec(NodoP reco, int valor) {
+            if (reco == null) {
+                return;
+            }
+            if (reco.Valor.Antiguedad > valor)
+            {
+                Buscarrec(reco.Izqu, valor);
+                Console.WriteLine(reco.Valor.Nombre + " ");
+                Buscarrec(reco.Dere, valor);
+            }
+            else {
+                Buscarrec(reco.Dere, valor);
+            }
+        }
+        public void Penultimo() {
+            if (Primero == null || (Primero.Izqu == null && Primero.Dere == null)){
+                Console.WriteLine("No existe");
+                return;
+            }
+            NodoP actual = Primero;
+            NodoP padre = null;
+            while (actual.Dere != null) {
+                padre = actual;
+                actual = actual.Dere;
+            }
+            if (actual.Izqu != null) {
+                NodoP temp = actual.Izqu;
+                while (temp.Dere != null) {
+                    temp = temp.Dere;
+                }
+                Console.WriteLine(temp.ToString();
+            }
+            else {
+                Console.WriteLine(padre.ToString();
+            }
+                            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBBP ArbolEmpleados = new ArbolBBP();
+            ArbolEmpleados.Agregar(3689,"Marlo",1);
+            ArbolEmpleados.Agregar(2378, "jua", 7);
+            ArbolEmpleados.Agregar(4975, "d2", 9);
+            ArbolEmpleados.Agregar(8162, "dagb",3);
+            ArbolEmpleados.Agregar(4763, "vvvv", 4);
+            ArbolEmpleados.Agregar(5915, "aaaa", 11);
+            ArbolEmpleados.Agregar(1584, "mmm", 10);
+            ArbolEmpleados.Agregar(5861, "dds", 6);
+            int op;
+            do
+            {
+                Console.WriteLine("\nLista de Opciones: ");
+                
+                
+                Console.WriteLine("1- Agregar");
+                Console.WriteLine("2- Listar1");
+                Console.WriteLine("3- Listar2");
+                Console.WriteLine("4- Mayores a cierta antiguedad");
+                Console.WriteLine("5- Penultimo segun el codigo");
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Ingrese una opciion: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op) {
+                    case 1:
+                        
+                        Console.WriteLine("Ingrese nombre: ");
+                        string nom = Console.ReadLine();
+                        Console.WriteLine("Ingrese codigo: ");
+                        int cod = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese nombre: ");
+                        int antiguedad = int.Parse(Console.ReadLine());
+                        ArbolEmpleados.Agregar(cod, nom, antiguedad);
+                        break;
+                    case 2:
+                        ArbolEmpleados.MuestraInCodigo();
+                        break;
+                    case 3:
+                        ArbolEmpleados.MuestraPosAntiguedad();
+                        break;
+                    case 4:
+                        Console.WriteLine("Ingrese un valor para buscar por antiguedad empleados mayores a: ");
+                        int anti = int.Parse(Console.ReadLine());
+                        ArbolEmpleados.MayoresA(anti);
+                        break;
+                    case 5:
+                        Console.WriteLine("Penultimo empleado: ");
+                        ArbolEmpleados.Penultimo();
+                        break;
+                    case 9:
+                        Console.WriteLine("Saliendo");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion correcta");
+                        break;
+                }
+
+
+
+            }
+            while (op!=9);
+            
+        }
+    }
+}
